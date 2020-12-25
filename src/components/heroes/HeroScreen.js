ba4a552f2fb0450145fb3a563a815f284f25182f
Glenda Guerrero <glendaguerrerof@gmail.com>
2020-12-25 03:58:46 -0500

    subiendo cambios

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -41,14 +41,14 @@ export const HeroScreen = ({history}) => {
                 className="img-thumbnail animate__animated animate__fadeInLeft"/>
             </div>
             <div className="col-8 animate__animated animate__fadeInLeft">
-                <h3>{superhero}</h3>
+                <h3 className='mr-2'>{superhero}</h3>
                 <ul className="list-group list-group-flush">
                     <li className="list-group-item">  <b>Alter ego:  </b>{alter_ego}  </li>
                     <li className="list-group-item">  <b>Publisher:  </b>{publisher}  </li>
                     <li className="list-group-item">  <b>First appearance:  </b>{first_appearance}  </li>
                 </ul>
 
-                <h5>Characters: </h5>
+                <h6 className='mr-2'>Characters: </h6>
                 <p>{characters}</p>
                 <button 
                 className="btn btn-outline-info mb-2"
